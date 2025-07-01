7eda28d38f2ef61d97aad90c70d0f6e26082e470
Vladislav Hnyloskurenko <v.hniloskurenko@gmail.com>
2025-07-01 13:15:09 +0300

    Added comments, test cases and a loop to run them (First-task)

--- first-task.py
+++ first-task.py
@@ -4,25 +4,26 @@ def caching_fibonacci() -> Callable[[int], int]:
 
     cache_dict = {}
 
-    def fibonacci(n:int) -> int:
+    def fibonacci(n: int) -> int:
         if n <= 0:
             return 0
         if n == 1:
             return 1
-        if n in cache_dict:
+        if n in cache_dict:     # Checking for the presence of a value in the cache
             return cache_dict[n]
         
+        # Adding a new key-value to the cache dictionary
         cache_dict[n] = fibonacci(n-1) + fibonacci(n-2)
-
         return cache_dict[n]
     
     return fibonacci
 
 
+
+test_cases = [10, 5, -1, 11, 20, 15]
+
 fib = caching_fibonacci()
 
-print(fib(10))
-print(fib(5))
-print(fib(-1))
-print(fib(11))
+for num in test_cases:
+    print(fib(num))
 
